4122f799ded0e4f23ff489fd64d1f6c03aaa6f3a
bisbaldi <annika.baldi@googlemail.com>
2018-03-14 15:44:40 +0100

    Grant rights to nodes

--- python_sources/master_node/scenario_orchestrator.py
+++ python_sources/master_node/scenario_orchestrator.py
@@ -1,6 +1,7 @@
 import rpyc
 from time import sleep
 from Savoir import Savoir
+from ..data_acquisition.data_acquisition import connect_to_multichain
 
 # addresses = []
 #
@@ -26,6 +27,9 @@ from Savoir import Savoir
 class ScenarioOrchestrator:
     def __init__(self):
         self.chain_nodes = []
+        self.chain_rpc = connect_to_multichain()
+        self.groups = {}
+
 
         number_slaves = 3
         print("Orchestrator is ready for connections")
@@ -36,6 +40,7 @@ class ScenarioOrchestrator:
         except ConnectionRefusedError:
             self.connect_to_slaves(number_slaves)
         self.get_slave_addresses()
+        self.grant_rights(1, ['receive','send'], 'Blubber')
 
     def connect_to_slaves(self, number_of_slaves):
         sleep(20)
@@ -50,6 +55,17 @@ class ScenarioOrchestrator:
         for chain_node in self.chain_nodes:
             print("############## Address new")
             print(chain_node.getaddresses())
+
+    def grant_rights(self, number, rights, label):
+        if number > len(self.chain_nodes):
+            print('Error not enough resources')
+        else:
+            self.groups[label] = []
+            for i in range(number):
+                for right in rights:
+                    self.chain_rpc.grant(self.chain_nodes[i].getaddresses()[0], right)
+                self.groups[label].append(self.chain_nodes.pop(0))
+
 
 
 if __name__ == '__main__':
--- shell_scripts/masterEntry.sh
+++ shell_scripts/masterEntry.sh
@@ -1,4 +1,4 @@
 #!/bin/sh +x
 multichain-util create bpchain -anyone-can-connect=true
 multichaind bpchain -printtoconsole &
-python3 python_sources/master_node/scenario_orchestrator.py
+python3 -m python_sources.master_node.scenario_orchestrator
