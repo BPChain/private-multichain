64256036e8402f6c8bfda8d6d85d23bd8513f2c7
Anton von Weltzien <anton.weltzien@gmail.com>
2018-03-14 15:44:15 +0100

    Allow connection to multiple slaves

--- python_sources/master_node/scenario_orchestrator.py
+++ python_sources/master_node/scenario_orchestrator.py
@@ -27,24 +27,28 @@ class ScenarioOrchestrator:
     def __init__(self):
         self.chain_nodes = []
 
-        number_slaves = 1
+        number_slaves = 3
         print("Orchestrator is ready for connections")
         try:
+            print("connectiong")
             self.connect_to_slaves(number_slaves)
+            print('connected')
         except ConnectionRefusedError:
             self.connect_to_slaves(number_slaves)
         self.get_slave_addresses()
 
     def connect_to_slaves(self, number_of_slaves):
         sleep(20)
-
-        connection = rpyc.connect('slavenode', 60000)
-        user, password, rpc_port = connection.root.get_credentials()
-        chain_node = Savoir(user, password, "slavenode", rpc_port, "bpchain")
-        self.chain_nodes.append(chain_node)
+        for number in range(1,number_of_slaves+1):
+            connection = rpyc.connect("privatemultichain_slavenode_"+str(number), 60000)
+            user, password, rpc_port = connection.root.get_credentials()
+            chain_node = Savoir(user, password, "privatemultichain_slavenode_"+str(number), rpc_port, "bpchain")
+            self.chain_nodes.append(chain_node)
+            print("################ Add connection")
 
     def get_slave_addresses(self):
         for chain_node in self.chain_nodes:
+            print("############## Address new")
             print(chain_node.getaddresses())
 
 
