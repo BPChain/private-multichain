c2df470d8eb4b84c2e64732da67cb4a09d661fb8
Anton von Weltzien <anton.weltzien@gmail.com>
2018-03-15 15:17:19 +0100

    Remove unused code

--- python_sources/master_node/scenario_orchestrator.py
+++ python_sources/master_node/scenario_orchestrator.py
@@ -5,43 +5,13 @@ from time import sleep
 from Savoir import Savoir
 from ..data_acquisition.data_acquisition import connect_to_multichain
 
-# addresses = []
-#
-#
-# def get_master_address():
-#     return str(master_rpc.getaddresses())[2:-2]
-#
-#
-# def generate_assets(asset_name, quantity, units):
-#     get_master_address()
-#     master_rpc.issue(get_master_address(), asset_name, quantity, units)
-#     print('Issue ' + str(quantity) + ' of asset ' + asset_name)
-#
-#
-# def grant_send_permission(address):
-#     master_rpc.grant(address, 'send')
-#
-#
-# def grant_receive_permission(address):
-#     master_rpc.grant(address, 'receive')
-
-
 class ScenarioOrchestrator:
     def __init__(self):
         self.chain_nodes = []
         self.chain_rpc = connect_to_multichain()
         self.groups = {}
-        number_slaves = 3
         print("Orchestrator is ready for connections")
         print("Starting to connect")
-        self.connect_to_slaves(number_slaves)
-        print('Connected all Slaves')
-        self.get_slave_addresses()
-        self.grant_rights(2, ['receive','send'], 'Students')
-        self.issue_assets('EVAPCoin', 100, 0.1)
-        self.send_assets(self.chain_rpc, self.groups['Students'][0], 'EVAPCoin', 10)
-        self.send_assets_to_group(self.chain_rpc, 'Students', 'EVAPCoin', 10)
-        self.revoke_rights('Students', ['receive'])
 
 
     def connect_to_slaves(self, number_of_slaves):
@@ -62,10 +32,6 @@ class ScenarioOrchestrator:
                 print("could not resolve name. removing id")
                 unconnected_ids.remove(slave_id)
 
-    def get_slave_addresses(self):
-        for chain_node in self.chain_nodes:
-            print("############## new Address")
-            print(chain_node.getaddresses())
 
     def grant_rights(self, number, rights, label):
         if number > len(self.chain_nodes):
