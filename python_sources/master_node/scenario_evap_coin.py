3dcb3fa79a301f0f395a3394c82bee5e29b721ff
bisbaldi <annika.baldi@googlemail.com>
2018-03-21 14:01:42 +0100

    Add is_blocked to remove sleeps
    Signed-off-by: bisbaldi <annika.baldi@googlemail.com>

--- python_sources/master_node/scenario_evap_coin.py
+++ python_sources/master_node/scenario_evap_coin.py
@@ -13,22 +13,17 @@ if __name__ == '__main__':
     orchestrator.grant_rights(5, ['receive'], 'Stores')
     orchestrator.issue_assets("EVAPCoin", 100, 1, True)
     orchestrator.issue_more('EVAPCoin', 20)
-    sleep(10)
     orchestrator.send_assets_to_group(orchestrator.chain_rpc, 'Students', 'EVAPCoin', 3)
-    sleep(10)
     print(orchestrator.get_quantity_of_asset(orchestrator.groups['Students'][3], 'EVAPCoin'))
     orchestrator.send_assets(orchestrator.groups['Students'][3],
-                             orchestrator.groups['Stores'][2], 'EVAPCoin', 5)
+                             orchestrator.groups['Stores'][2], 'EVAPCoin', 3)
     orchestrator.send_assets(orchestrator.groups['Students'][4],
                              orchestrator.groups['Stores'][0], 'EVAPCoin', 3)
-    sleep(10)
     print(orchestrator.get_quantity_of_asset(orchestrator.groups['Students'][3], 'EVAPCoin'))
     while True:
-        sleep(10)
         if orchestrator.get_quantity_of_asset(orchestrator.groups['Students'][0], 'EVAPCoin') >= 2:
             orchestrator.send_assets(orchestrator.groups['Students'][0],
                                      orchestrator.groups['Stores'][1], 'EVAPCoin', 2)
-        sleep(10)
         if orchestrator.get_quantity_of_asset(orchestrator.groups['Students'][1], 'EVAPCoin') >= 2:
             orchestrator.send_assets(orchestrator.groups['Students'][1],
                                      orchestrator.groups['Stores'][0], 'EVAPCoin', 2)
