f7ea70117e447dab48ecd47c8ae08ddb69eb16a0
Anton von Weltzien <anton.weltzien@gmail.com>
2018-04-12 13:22:59 +0200

    Add average blocksize

--- python_sources/data_acquisition/data_acquisition.py
+++ python_sources/data_acquisition/data_acquisition.py
@@ -5,6 +5,7 @@ import os
 import time
 from configparser import ConfigParser
 from typing import Tuple
+from statistics import mean
 
 import yaml
 from Savoir import Savoir
@@ -40,13 +41,24 @@ def get_node_data(chain_node, last_block_number, hostname):
     difficulty = float(chain_node.getmininginfo()['difficulty'])
     hashespersec = int(chain_node.getmininginfo()['hashespersec'])
     is_mining = 0 if hashespersec == 0 else 1  # TODO: replace with 'correct' request
+    avg_blocksize = calculate_avg_blocksize(chain_node,last_block_number)
     avg_blocktime, new_last_block_number = calculate_avg_blocktime(chain_node, last_block_number)
     LOG.info({difficulty, hashespersec, is_mining, avg_blocktime})
     # TODO include blockSize
     return {'target': hostname, 'chainName': 'multichain', 'hostId': chain_node.getaddresses()[0],
-            'hashrate': hashespersec, 'blockSize': -1,
+            'hashrate': hashespersec, 'blockSize': avg_blocksize,
             'avgDifficulty': difficulty, 'avgBlocktime': avg_blocktime,
             'isMining': is_mining}, new_last_block_number
+
+
+def calculate_avg_blocksize(chain_node, last_block_number):
+    newest_block_number = chain_node.getblockchaininfo()['blocks']
+    if last_block_number < newest_block_number:
+        sizes = [chain_node.getblock(block_number)['size'] for block_number in
+                 range(last_block_number, newest_block_number + 1)]
+        return mean(sizes)
+    else:
+        return chain_node.getblock(last_block_number)['size']
 
 
 def calculate_avg_blocktime(chain_node, last_block_number):
@@ -106,7 +118,7 @@ def main():
     hostname = os.environ["TARGET_HOSTNAME"]
     time.sleep(15)  # sleep so we hopefully mine a block. TODO: replace with safe
     # implementation
-    send_period = 100
+    send_period = 15
     rpc_api = connect_to_multichain()
     provide_data_every(send_period, rpc_api, hostname)
 
