c526bda8937e47e02c86e85d2f8dfb73df32680d
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-28 11:39:42 +0200

    Adapt to new Adapter structure

--- python_sources/data_acquisition/multichian_adapter.py
+++ python_sources/data_acquisition/multichian_adapter.py
@@ -7,11 +7,19 @@ from python_sources.data_acquisition.multichain_connector import connect_to_mult
 
 
 class MultichainAdapter(BlockchainAdapter):
-
     def __init__(self, is_miner):
         super().__init__(is_miner)
         self.rpc_client = connect_to_multichain()
-        self.previous_block_number = 0
+
+    def fetch_newest_block_number(self) -> int:
+        return self.rpc_client.getblockchaininfo()['blocks']
+
+    def fetch_block_with(self, number: int):
+        return self.rpc_client.getblock(str(number))
+
+    def make_block_from(self, raw_block: dict) -> Block:
+        return Block(raw_block['difficulty'], raw_block['tx'],
+                     raw_block['time'], raw_block['size'])
 
     def hashrate(self) -> int:
         return int(self.rpc_client.getmininginfo()['hashespersec'])
@@ -23,19 +31,3 @@ class MultichainAdapter(BlockchainAdapter):
 
     def host_id(self):
         return self.rpc_client.getaddresses()[0]
-
-    def new_blocks_and_previous(self) -> Tuple[List[Block], Block]:
-        newest_block_number = self.rpc_client.getblockchaininfo()['blocks']
-        raw_blocks = [self.rpc_client.getblock(str(number))
-                       for number in range(self.previous_block_number, newest_block_number + 1)]
-
-        def make_block_from(raw_block: dict):
-            return Block(raw_block['difficulty'], raw_block['tx'],
-                         raw_block['time'], raw_block['size'])
-
-        blocks = [make_block_from(raw_block) for raw_block in raw_blocks]
-        old_block = blocks[0]
-        new_blocks = blocks[1:] if len(blocks) > 1 else []
-        return new_blocks, old_block
-
-
