90368cbd0fe0a2fd7d1c3fa7b58fa1fc165f20c5
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-30 11:17:03 +0200

    Update Sender to newer version

--- python_sources/data_acquisition/data_collection.py
+++ python_sources/data_acquisition/data_collection.py
@@ -11,11 +11,9 @@ from python_sources.data_acquisition.multichian_adapter import MultichainAdapter
 
 def main():
     is_miner = sys.argv[1] if len(sys.argv) > 1 else '1'
-    print(is_miner)
     uri = yaml.safe_load(open('/python_sources/data_acquisition/config.yml'))
     server_address = uri['serverAddress']
-    blockchain_reader = BlockchainReader('multichaind', 'multichain', MultichainAdapter(is_miner))
-    Sender(server_address, 15, blockchain_reader)
+    Sender(server_address, 'multichaind', 'multichain', MultichainAdapter(is_miner))
 
 
 if __name__ == '__main__':
