32599177a9119129ad0ba838844acdb89fd9ae9d
jcremerius <jonas.cremerius@student.hpi.de>
2018-06-26 13:16:37 +0200

    Modify target-block-time and maximum-block-size

--- shell_scripts/masterEntry.sh
+++ shell_scripts/masterEntry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash +x
 multichain-util create bpchain -anyone-can-connect=true
+echo "target-block-time = 10\nmaximum-block-size = 1000000000 " >> /root/.multichain/bpchain/params.dat
 multichaind bpchain -printtoconsole &
 python3 -m python_sources.data_acquisition.data_collection 0 &
 python3 -m python_sources.master.run_scenario_service
